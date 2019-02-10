ed8299767bce736eda2c8e73ed70d7747772dbfa
2ps <pshingavi@gmail.com>
2019-02-10 11:27:53 -0600

    Made aws reliant tests configurable via environment

--- tests/test_aws.py
+++ tests/test_aws.py
@@ -11,41 +11,48 @@ __all__ = [
 
 class Aws(TestCase):
     plaintext = 'hello, peanut!'
+    profile = 'test'
 
     def setUp(self):
-        region = os.environ.get('AWS_REGION', 'us-east-2')
-        access_key_id = os.environ.get('AWS_ACCESS_KEY_ID', 'cody')
-        secret_access_key = os.environ.get('AWS_SECRET_ACCESS_KEY', 'jinx')
-        handle = Popen([
-            '/bin/bash',
-            '-c',
-            f'source bin/activate '
-            f'  && aws --profile=company configure set region {region}'
-            f'  && aws --profile=company configure '
-            f'set aws_access_key_id {access_key_id}'
-            f'  && aws --profile=company configure set '
-            f'aws_secret_access_key {secret_access_key}' ])
-        handle.communicate()
+        region = os.environ.get('AWS_REGION')
+        access_key_id = os.environ.get('AWS_ACCESS_KEY_ID')
+        secret_access_key = os.environ.get('AWS_SECRET_ACCESS_KEY')
+        values = [
+            ('region', region),
+            ('aws_access_key_id', access_key_id),
+            ('aws_secret_access_key', secret_access_key),
+        ]
+        for key, value in values:
+            if not value:
+                continue
+            handle = Popen([
+                '/bin/bash',
+                '-c',
+                f'source bin/activate '
+                f'  && aws --profile={self.profile} '
+                f'configure set {key} {value}',
+            ])
+            handle.communicate()
 
     def kms(self):
         from murmuration.kms import encrypt
         from murmuration.kms import decrypt
-        value = encrypt(self.plaintext, 'dev', 'us-east-2', 'company')
-        value = decrypt(value, 'us-east-2', 'company')
+        value = encrypt(self.plaintext, 'dev', profile=self.profile)
+        value = decrypt(value, profile=self.profile)
         self.assertEqual(value, self.plaintext)
 
     def kms_wrapped(self):
         from murmuration.kms_wrapped import encrypt
         from murmuration.kms_wrapped import decrypt
-        value = encrypt(self.plaintext, 'dev', 'us-east-2', 'company')
-        value = decrypt(value, 'us-east-2', 'company')
+        value = encrypt(self.plaintext, 'dev', profile=self.profile)
+        value = decrypt(value, profile=self.profile)
         self.assertEqual(value, self.plaintext)
 
     def test_session(self):
-        session = cached_session('us-east-2', 'company')
-        self.assertEqual(session.profile_name, 'company')
+        session = cached_session(region='us-east-2', profile=self.profile)
+        self.assertEqual(session.profile_name, self.profile)
         self.assertEqual(session.region_name, 'us-east-2')
-        x = cached_session('us-east-2', 'company')
+        x = cached_session(region='us-east-2', profile=self.profile)
         self.assertIs(session, x)
         self.kms()
         self.kms_wrapped()
